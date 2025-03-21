6f3a96b64cf3bc79161c2a3b2cd56ae591ea4cef
Andrey Kozhuhov <andrpre96@gmail.com>
2025-03-21 23:03:25 +0300

    feat: add Button, Paragraph, Title components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Title from "./components/Title/Title";
+import Paragraph from "./components/Paragraph/Paragraph";
+import Button from "./components/Button/Button";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Title text="Поиск" />
+      <Paragraph text="Введите название фильма, сериала или мультфильма для поиска и добавления в избранное." />
+      <Button text="Искать" />
+    </>
+  );
 }
 
 export default App;

480e03b967a90c04480481f0b4b87f50daef711c
Andrey Kozhuhov <andrpre96@gmail.com>
2025-04-30 23:16:55 +0300

    feat: added header card

--- src/pages/CardPage/CardPage.tsx
+++ src/pages/CardPage/CardPage.tsx
@@ -2,12 +2,29 @@ import { FC } from "react";
 import { useLoaderData } from "react-router";
 
 import Title from "../../components/Title/Title";
+
 import { IMovie } from "../../types/apiData";
+import styles from "./CardPage.module.css";
 
 const CardPage: FC = () => {
   const movie = useLoaderData() as IMovie;
 
-  return <Title text={movie.short.name} />;
+  return (
+    <div>
+      <div className={styles.titleWrapper}>
+        <span className={styles.titleAdditional}>Поиск фильмов</span>
+        <Title
+          text={movie.short.name}
+          style={{ fontSize: "var(--font-size-large)", lineHeight: "var(--line-height-large)" }}
+        />
+      </div>
+
+      <div>
+        <img src={movie.short.image} alt={movie.short.name} />
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default CardPage;
